ace4cb06810b2e03f09c419902b08f7785c8b3e0
Crow314 <master@crow31415.net>
2022-05-11 17:43:38 +0900

    Solved Week03-F

--- Week03/F/main.py
+++ Week03/F/main.py
@@ -6,37 +6,23 @@ if __name__ == '__main__':
     for i in range(N):
         height = int(input())
         if i % 2 == 0:
-            for j in range(0, height):
-                obstacles[j][1] += 1
+            obstacles[0][1] += 1
+            obstacles[height-1][1] += -1
         else:
-            for j in range(H - height, H):
-                obstacles[j][1] += 1
+            obstacles[H-height-1][1] += 1
+
+    for i in range(1, H):
+        obstacles[i][1] += obstacles[i-1][1]
 
     obstacles.sort(key=lambda x: x[1])
 
     level = obstacles[0][1]
     count = 0
-    idx = 0
 
-    # bisection search
-    left = 0
-    right = H - 1
-    while True:
-        if right - left <= 1:
-            break
-
-        idx = int((left + right) / 2)
-
-        if obstacles[idx][1] == level:
-            left = idx
-            continue
-        if obstacles[idx][1] > level:
-            right = idx
-            continue
-
-    for i in range(idx-1, N):
-        if obstacles[i][1] != level:
-            count = i
+    for obstacle in obstacles:
+        if obstacle[1] == level:
+            count += 1
+        else:
             break
 
     print(str(level) + ' ' + str(count))
