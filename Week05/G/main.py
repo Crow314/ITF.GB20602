138c21d0e463e4b70843fc8cc676a03ab0f1b9de
Crow314 <master@crow31415.net>
2022-05-24 21:48:45 +0900

    Tried to solve Week05-G (TLE) (2nd)

--- Week05/G/main.py
+++ Week05/G/main.py
@@ -1,44 +1,53 @@
-import sys
+from collections import deque
 
 
-def dfs(graph: list, groups: list, host: int, group: int, not_bipartite: bool) -> bool:
-    groups[host] = group
+def bfs(graph: list, groups: list, host: int) -> bool:
+    groups[host] = 1
 
-    for next_host in graph[host]:
-        next_group = groups[next_host]
-        if next_group == 0:
-            if group == 1:
-                next_group = 2
-            else:
-                next_group = 1
+    bfs_queue = deque()
+    bfs_queue.append(host)
 
-            not_bipartite = dfs(graph, groups, next_host, next_group, not_bipartite)
-        elif next_group == group:
-            not_bipartite = True
+    bipartite = True
 
-    return not_bipartite
+    while len(bfs_queue) > 0:
+        host = bfs_queue.popleft()
+        group = groups[host]
+
+        for next_host in graph[host]:
+            next_group = groups[next_host]
+
+            if next_group == 0:
+                if group == 1:
+                    next_group = 2
+                else:
+                    next_group = 1
+
+                bfs_queue.append(next_host)
+                groups[next_host] = next_group
+            elif next_group == group:
+                bipartite = False
+
+    return bipartite
 
 
 if __name__ == '__main__':
     N, M = map(int, input().split())
     graph = [[] for _ in range(N)]
-    groups = [False for _ in range(N)]  # 0: not visited / 1: Group1 / 2: Group2
+    groups = [False for _ in range(N)]  # 0: not queued / 1: Group1 / 2: Group2
 
     for _ in range(M):
         u, v = map(int, input().split())
         graph[u-1].append(v-1)
         graph[v-1].append(u-1)  # Undirected Graph
 
-    sys.setrecursionlimit(M * 10)
-
     count = -1
     has_not_bipartite = False
     while True:
         try:
             host = groups.index(False)
-            not_bipartite = dfs(graph, groups, host, 1, False)
+            bipartite = bfs(graph, groups, host)
 
-            if not_bipartite:
+            if not bipartite:
                 has_not_bipartite = True
         except ValueError:
             break
