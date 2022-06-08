dc1f91ac668b255bdb6a0d49a9f9ba606b6c9842
Crow314 <master@crow31415.net>
2022-06-08 19:25:20 +0900

    Solved Week06-F

--- Week06/F/main.py
+++ Week06/F/main.py
@@ -1,83 +1,124 @@
 import math
-from collections import deque
+from numbers import Number
+from copy import deepcopy
+
+INF = float('inf')
 
 
-def adj_list(rooms: list) -> list:
+class MaxFlow:
+    def __init__(self, V: int):
+        self.V = V
+        self.EL = []
+        self.AL = [list() for _ in range(self.V)]
+        self.d = []
+        self.last = []
+        self.p = []
+        self.has_been_run = False
+
+    def BFS(self, s: int, t: int) -> bool:
+        self.d = [-1] * self.V
+        self.d[s] = 0
+        self.p = [[-1, -1] for _ in range(self.V)]
+        q = [s]
+        while len(q) != 0:
+            u = q[0]
+            q.pop(0)
+            if u == t:
+                break
+            for idx in self.AL[u]:
+                v, cap, flow = self.EL[idx]
+                if cap - flow > 0 and self.d[v] == -1:
+                    self.d[v] = self.d[u]+1
+                    q.append(v)
+                    self.p[v] = [u, idx]
+        return self.d[t] != -1
+
+    def send_one_flow(self, s: int, t: int, f: Number = INF) -> Number:
+        if s == t:
+            return f
+        u, idx = self.p[t]
+        _, cap, flow = self.EL[idx]
+        pushed = self.send_one_flow(s, u, min(f, cap-flow))
+        flow += pushed
+        self.EL[idx][2] = flow
+        self.EL[idx ^ 1][2] -= pushed
+        return pushed
+
+    def DFS(self, u: int, t: int, f: Number = INF) -> Number:
+        if u == t or f == 0:
+            return f
+        for i in range(self.last[u], len(self.AL[u])):
+            self.last[u] = i
+            v, cap, flow = self.EL[self.AL[u][i]]
+            if self.d[v] != self.d[u]+1:
+                continue
+            pushed = self.DFS(v, t, min(f, cap - flow))
+            if pushed != 0:
+                flow += pushed
+                self.EL[self.AL[u][i]][2] = flow
+                self.EL[self.AL[u][i] ^ 1][2] -= pushed
+                return pushed
+        return 0
+
+    def add_edge(self, u: int, v: int, capacity: Number,
+                 directed: bool = True) -> None:
+        if u == v:
+            return
+        self.EL.append([v, capacity, 0])
+        self.AL[u].append(len(self.EL)-1)
+        self.EL.append([u, 0 if directed else capacity, 0])
+        self.AL[v].append(len(self.EL)-1)
+
+    def assert_has_not_already_been_run(self):
+        if self.has_been_run:
+            msg = ('Rerunning a max flow algorithm on the same graph will '
+                   + 'result in incorrect behaviour. Please use .copy() '
+                   + 'before you run any max flow algorithm if you need to '
+                   + 'run multiple iterations')
+            raise Exception(msg)
+
+        self.has_been_run = True
+
+    def dinic(self, s: int, t: int) -> Number:
+        """
+        Returns the max flow obtained by running Dinic's algorithm.
+        Modifies the graph in place.
+        Arguments:
+            `s`: The source vertex.
+            `t`: The sink vertex.
+        Returns:
+            The max flow.
+        """
+        self.assert_has_not_already_been_run()
+
+        mf = 0
+        while self.BFS(s, t):
+            self.last = [0] * self.V
+            f = self.DFS(s, t)
+            while f != 0:
+                mf += f
+                f = self.DFS(s, t)
+        return mf
+
+    def copy(self) -> 'MaxFlow':
+        return deepcopy(self)
+
+    def __repr__(self) -> str:
+        el = self.EL[:10] + ['...'] if len(self.EL) > 10 else self.EL
+        al = self.AL[:10] + ['...'] if len(self.AL) > 10 else self.AL
+        el = ', '.join(map(str, el))
+        al = ', '.join(map(str, al))
+        return f'MaxFlow(V={self.V}, EL=[{el}], AL=[{al}])'
+
+
+def adj_list(mf: MaxFlow, rooms: list):
     n = len(rooms)
-    graph = [[] for _ in range(n)]
 
     for i in range(n):
         for j in range(i+1, n):
             cap = math.gcd(rooms[i], rooms[j])
             if cap > 1:
-                edge1 = [j, cap, None]
-                edge2 = [i, cap, edge1]
-                edge1[2] = edge2
-
-                graph[i].append(edge1)
-                graph[j].append(edge2)
-
-    return graph
-
-
-def bfs(graph: list, source: int, sink: int) -> list:
-    n = len(graph)
-    levels = [-1 for _ in range(n)]
-    bfs_queue = deque()
-
-    levels[source] = 0
-    bfs_queue.append(source)
-
-    while len(bfs_queue) > 0:
-        vertex = bfs_queue.popleft()
-        next_level = levels[vertex] + 1
-
-        for next_vertex, cap, _ in graph[vertex]:
-            if (levels[next_vertex] == -1) and (cap > 0):
-                levels[next_vertex] = next_level
-                bfs_queue.append(next_vertex)
-
-    return levels
-
-
-def dfs(graph: list, levels: list, iterator: list, vertex: int, sink: int, flow: int) -> int:
-    if vertex == sink:
-        return flow
-
-    for i in range(max(iterator[vertex] - 1, 0), len(graph[vertex])):
-        iterator[vertex] = i
-        edge = graph[vertex][i]
-        dst, cap, rev_edge = edge
-
-        if levels[dst] != levels[vertex] + 1:
-            continue
-
-        if levels[vertex] < levels[dst]:
-            f = dfs(graph, levels, iterator, dst, sink, min(flow, cap))
-            if f != 0:
-                edge[1] -= f
-                rev_edge[1] += f
-                return f
-
-    return 0
-
-
-def max_flow(graph: list, source: int, sink: int) -> int:  # Dinic's algorithm
-    flow = 0
-    INF = 10 ** 10
-
-    while True:
-        levels = bfs(graph, source, sink)
-        if levels[sink] == -1:
-            break
-
-        itr = [0 for _ in range(len(graph))]
-        f = INF
-        while f != 0:
-            f = dfs(graph, levels, itr, source, sink, INF)
-            flow += f
-
-    return flow
+                mf.add_edge(i, j, cap, False)
 
 
 def main():
@@ -102,8 +143,9 @@ def main():
             max_vertex = weight
             goal = i
 
-    graph = adj_list(rooms)
-    print(max_flow(graph, start, goal))
+    mf = MaxFlow(n)
+    adj_list(mf, rooms)
+    print(mf.dinic(start, goal))
 
 
 if __name__ == '__main__':
