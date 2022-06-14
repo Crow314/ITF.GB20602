0e965a9f8e8d4d74fb918b1d651d58ebc2cbd4f8
Crow314 <master@crow31415.net>
2022-06-15 02:14:50 +0900

    [RTE] Week07-C (3rd)

--- Week07/C/main.py
+++ Week07/C/main.py
@@ -8,26 +8,34 @@ def main():
     for _ in range(n):
         number = list(map(int, list(input())))
         l = len(number)
-        node = root
+        parent_node = root
 
         for i in range(l):
             c = number[i]
 
-            if c not in node.keys():
-                node[c] = {}
-            next_node = node[c]
+            if c in parent_node.keys():
+                node = parent_node[c]
 
-            if -1 in next_node.keys():
-                consistent = False
-                break
-
-            if i == l-1:  # last char
-                if (-1 not in next_node.keys()) and (len(next_node) > 0):  # has next node
+                if -1 in node.keys():
                     consistent = False
                     break
-                next_node[-1] = None
+
+                if i == l-1:  # last char
+                    if (-1 not in node.keys()) and (len(node) > 0):  # has next node
+                        consistent = False
+                        break
+                    node[-1] = None
+                else:
+                    parent_node = node
+
             else:
-                node = next_node
+                parent_node[c] = {}
+                node = parent_node[c]
+
+                if i == l-1:  # last char
+                    node[-1] = None
+                else:
+                    parent_node = node
 
         if not consistent:
             break
