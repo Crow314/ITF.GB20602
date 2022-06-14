ee9fe11787817ddadcc55f8e9d31683acd1c367d
Crow314 <master@crow31415.net>
2022-06-15 00:07:16 +0900

    [TLE] Week07-I

--- Week07/I/main.py
+++ Week07/I/main.py
@@ -1,29 +1,16 @@
 def LCS(str1: list, str2: list) -> int:
-    dp = [[0 for _ in range(len(str2)+1)] for _ in range(len(str1)+1)]
+    dp = [[0 for _ in range(len(str2)+1)] for _ in range(2)]
 
     for i, v_i in enumerate(str1):
         for j, v_j in enumerate(str2):
             if v_i == v_j:
-                dp[i+1][j+1] = dp[i][j] + 1
+                dp[1][j+1] = dp[0][j] + 1
             else:
-                dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1])
+                dp[1][j+1] = max(dp[1][j], dp[0][j+1])
 
-    ans = []
+        dp[0] = dp[1]
 
-    i = len(str1) - 1
-    j = len(str2) - 1
-
-    while i >= 0 and j >= 0:
-        if str1[i] == str2[j]:
-            ans.append(str1[i])
-            i -= 1
-            j -= 1
-        elif dp[i+1][j+1] == dp[i][j+1]:
-            i -= 1
-        elif dp[i+1][j+1] == dp[i+1][j]:
-            j -= 1
-
-    return len(ans)
+    return dp[0][len(str2)]
 
 
 def main() -> int:
