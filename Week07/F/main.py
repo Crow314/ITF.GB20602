990e3da682d329841d7ce2acb8c487c79d2b8266
Crow314 <master@crow31415.net>
2022-06-15 04:30:07 +0900

    [TLE] Week07-F

--- Week07/F/main.py
+++ Week07/F/main.py
@@ -1,9 +1,18 @@
-import re
-
-
 def main(string: str):
-    substr = re.match(r'\A(.+?)\1*\Z', string).groups()[0]
-    print(int(len(string) / len(substr)))
+    str_len = len(string)
+    ans = 0
+
+    for i in range(1, str_len+1):
+        if str_len % i != 0:
+            continue
+
+        ans = int(str_len / i)
+        substr = string[:i]
+
+        if substr * ans == string:
+            break
+
+    print(ans)
 
 
 if __name__ == '__main__':
